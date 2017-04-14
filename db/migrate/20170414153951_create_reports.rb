2d9ad71ddda0bc4b42a6d0977170baf7045637c7
Dan McKeon <email": null>
2017-04-14 16:17:56 -0700

    add all reports from one page to database

--- db/migrate/20170414153951_create_reports.rb
+++ db/migrate/20170414153951_create_reports.rb
@@ -1,13 +1,13 @@
 class CreateReports < ActiveRecord::Migration
   def change
     create_table :reports do |t|
-      t.datetime :sighted
+      t.string :sighted # variable format for input data, potentially switch to t.date later
       t.string :city
       t.string :state
       t.string :shape
       t.string :duration
       t.string :summary
-      t.date :posted
+      t.string :posted # variable format for input data, potentially switch to t.date later
       t.timestamps null: false
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 require 'open-uri'
 require 'nokogiri'
 
+Report.destroy_all
+
 reports_by_date_noko = Nokogiri::HTML(open('http://www.nuforc.org/webreports/ndxpost.html'))
 
 links = reports_by_date_noko.css('td a').map { |element| "http://www.nuforc.org/webreports/#{element[:href]}" }
@@ -9,19 +11,18 @@ single_report_date_noko = Nokogiri::HTML(open('http://www.nuforc.org/webreports/
 
 single_reports = single_report_date_noko.css('tbody tr')
 
-reports_data = []
-
 single_reports.each do |report|
   data_elements = report.css('td')
-  Report.new
-  single_report_data[:sighted] = data_elements[0].text
-  single_report_data[:city] = data_elements[1].text
-  single_report_data[:state] = data_elements[2].text
-  single_report_data[:shape] = data_elements[3].text
-  single_report_data[:duration] = data_elements[4].text
-  single_report_data[:summary] = data_elements[5].text
-  single_report_data[:posted] = data_elements[6].text
-  reports_data << single_report_data
+  Report.create(sighted: data_elements[0].text,
+                city: data_elements[1].text,
+                state: data_elements[2].text,
+                shape: data_elements[3].text,
+                duration: data_elements[4].text,
+                summary: data_elements[5].text,
+                posted: data_elements[6].text)
 end
 
-reports_data.each { |report| p report}
+Report.all.each do |report|
+  p report
+end
+
