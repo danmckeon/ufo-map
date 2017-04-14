ebee0a1cd63a1c6cfc274a9c52bf708051b4a995
Dan McKeon <email": null>
2017-04-14 16:50:06 -0700

    add calculate frequencies

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,6 @@
 class Report < ActiveRecord::Base
-  # Remember to create a migration!
+  def self.calculate_frequencies
+    frequencies = Report.group("state").group("city").order("count_id desc").count("id")
+
+  end
 end
